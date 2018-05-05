36f6aff93ac4ac9107366227de809de0b23dc17e
Derek Hancock <derekhancock44@gmail.com>
2018-05-05 09:47:59 -0500

    make sure to shuffle

--- create_train_test_split.py
+++ create_train_test_split.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from random import shuffle
 
 dir_path = "review_polarity/txt_sentoken/"
 pos_path = dir_path + "pos/"
@@ -20,7 +21,9 @@ test_negative_review_files = np.random.choice(negative_review_files,size=int(len
 train_negative_review_files = [x for x in negative_review_files if x not in test_negative_review_files]
 
 train_review_files = train_positive_review_files + train_negative_review_files
+shuffle(train_review_files)
 test_review_files = test_positive_review_files + test_negative_review_files
+shuffle(test_review_files)
 
 #save list of files in each split
 train_file = open('review_polarity/train_file_paths.txt', 'w+')
