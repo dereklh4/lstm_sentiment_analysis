0cfe140ee2f66c3e7ffec70f61cf7a3b22965524
Derek Hancock <derekhancock44@gmail.com>
2018-05-05 10:33:30 -0500

    undo misguided change

--- create_train_test_split.py
+++ create_train_test_split.py
@@ -21,9 +21,7 @@ test_negative_review_files = np.random.choice(negative_review_files,size=int(len
 train_negative_review_files = [x for x in negative_review_files if x not in test_negative_review_files]
 
 train_review_files = train_positive_review_files + train_negative_review_files
-shuffle(train_review_files)
 test_review_files = test_positive_review_files + test_negative_review_files
-shuffle(test_review_files)
 
 #save list of files in each split
 train_file = open('review_polarity/train_file_paths.txt', 'w+')
